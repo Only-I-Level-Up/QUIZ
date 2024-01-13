0c06de602d9b169f471b4b739ca0c5d7c66fe6b6
lsj0202 <w1004123@gmail.com>
2024-01-13 23:41:15 +0900

    feat: add AppButton

---
+++ src/components/AppButton/index.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@chakra-ui/react";
+import React from "react";
+
+type AppButtonProps = {
+  value: string;
+  colorScheme: string;
+  variant: string;
+  className?: string;
+  disabled?: boolean;
+  width?: string;
+  onClick: (e?: React.MouseEvent<HTMLButtonElement>) => void;
+};
+
+const AppButton = ({
+  value,
+  colorScheme,
+  variant,
+  className,
+  disabled,
+  width,
+  onClick,
+}: AppButtonProps) => {
+  return (
+    <Button
+      onClick={onClick}
+      colorScheme={colorScheme}
+      variant={variant}
+      className={className}
+      disabled={disabled}
+      w={width}
+    >
+      {value}
+    </Button>
+  );
+};
+
+export default AppButton;
