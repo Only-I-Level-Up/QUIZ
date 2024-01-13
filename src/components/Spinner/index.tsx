7481cc96708b0f90cc155493aec8d6ed2c4480d5
lsj0202 <w1004123@gmail.com>
2024-01-14 00:13:01 +0900

    feat: add spinner

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+type SpinnerProps = {
+  thickness: string;
+  speed: string;
+  color: string;
+  emptyColor: string;
+  size: string;
+};
 
-const Spinner = () => {
-  return <div></div>;
+const Spinner = ({
+  thickness,
+  speed,
+  color,
+  emptyColor,
+  size,
+}: SpinnerProps) => {
+  return (
+    <Spinner
+      thickness={thickness}
+      speed={speed}
+      color={color}
+      emptyColor={emptyColor}
+      size={size}
+    />
+  );
 };
 
 export default Spinner;
